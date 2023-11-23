d0714c282aec4b9195244cbe14672fa2264cafaa
Jashan <jashanpreet28122001@gmail.com>
2023-11-22 22:50:04 -0600

    Backend - Complete

---
+++ src/main/java/com/example/yourjourneybackend/NewjourneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.yourjourneybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewjourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewjourneyApplication.class, args);
+	}
+
+}
